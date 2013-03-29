c32c1aa4040188717234e4bb6b5b2642d7ac8f27
Lucas Oliveira <luksrn@gmail.com>
2013-03-29 18:34:07 -0300

    Creating a navigation between the activities list of articles downloaded and also displaying screen downloads. I did a change in the default configuration of the eclipse to allow the use of a new API for this, just change in: Window -> Preferences -> Android -> Error Checking And Lint mark the serverity of the use of new API to warning.

--- src/br/ufrn/dimap/pubshare/activity/ArticleListActivity.java
+++ src/br/ufrn/dimap/pubshare/activity/ArticleListActivity.java
@@ -2,14 +2,16 @@ package br.ufrn.dimap.pubshare.activity;
 
 import java.util.List;
 
-import br.ufrn.dimap.pubshare.adapters.ArticleListAdapter;
-import br.ufrn.dimap.pubshare.domain.Article;
-import br.ufrn.dimap.pubshare.mocks.ArticleListMockFactory;
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.ListView;
+import br.ufrn.dimap.pubshare.adapters.ArticleListAdapter;
+import br.ufrn.dimap.pubshare.domain.Article;
+import br.ufrn.dimap.pubshare.mocks.ArticleListMockFactory;
 
 /**
  * Responsible for managing the activity of displaying articles available.
@@ -17,6 +19,8 @@ import android.widget.ListView;
  * @author Lucas Farias de Oliveira <i>luksrn@gmail.com</i>
  */
 public class ArticleListActivity extends Activity {
+	
+	private static final String TAG = "br.ufrn.dimap.pubshare.activity.ArticleListActivity";
 
 	private ListView articlesListView;
 	private ArticleListAdapter adapter;
@@ -26,7 +30,7 @@ public class ArticleListActivity extends Activity {
 		super.onCreate(savedInstanceState);				
 
 		setContentView(R.layout.activity_article_list);
-		setTitle(R.string.title_activity_article_list);
+		setTitle(R.string.title_activity_article_list);	 
 		
 		List<Article> articles = ArticleListMockFactory.makeArticleList();		
 		
@@ -41,8 +45,26 @@ public class ArticleListActivity extends Activity {
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
-		getMenuInflater().inflate(R.menu.article_list, menu);
+		getMenuInflater().inflate(R.menu.main_menu, menu);
 		return true;
 	}
-
+	
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch ( item.getItemId() ) {
+			case R.id.menu_my_downloads:
+				showMyDownloadActivity();
+				return true;	
+			default:
+		        return super.onOptionsItemSelected(item);
+		}
+	}
+	
+	public void showMyDownloadActivity(){
+		Log.d(TAG, "Display My Downloads Activity");
+		
+		Intent intent = new Intent(this, ArticlesDownloadedActivity.class);		
+		startActivity(intent);
+	}
+ 
 }
